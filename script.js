04c3d951aa9e1e91bc7878ea635b231bf3c08780
2012gemin-dotcom <2012gemin@gmail.com>
2026-08-10 13:01:58 +0400

    xvzx

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
+let metn = prompt("Lütfən mətn daxil edin:");
 
-document.addEventListener("DOMContentLoaded", function () {
-    
-    const tarix = prompt("Tarixi daxil edin (İl-Ay-Gün formatında, məsələn: 2026-10-25):");
-
-    if (tarix) {
-        
-        const [il, ay, gun] = tarix.split("-");
-
-        if (il && ay && gun) {
-            document.getElementById("netice").innerText = `Nəticə: ${gun}-${ay}-${il}`;
-        } else {
-            document.getElementById("netice").innerText = "Format səhvdir! Lütfən İl-Ay-Gün şəklində yazın.";
-        }
-    } else {
-        document.getElementById("netice").innerText = "Hər hansı bir tarix daxil edilmədi.";
-    }
-
-});
+if (/\d/.test(metn)) {
+    console.log("Mətndə rəqəm var.");
+} else {
+    console.log("Mətndə rəqəm yoxdur.");
+}
